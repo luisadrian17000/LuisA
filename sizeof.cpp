226bef0aa82a0c20907ba09dc8377b1e4a0370e6
LuisAdrian <luisadriandsg@gmail.com>
2023-05-10 16:28:08 -0600

    commit 1, añadi los 3 archivos.

---
+++ sizeof.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <climits>
+#include <cfloat>
+using namespace std;
+
+int main(){
+
+	cout << "int: " << sizeof(int) << endl;
+	cout << "bool: " << sizeof(bool) << endl;
+	cout << "char: " << sizeof(char) << endl;
+	cout << "float: " << sizeof(float) << endl;
+	cout << "double: " << sizeof(double) << endl;
+	cout << "long long int: " << sizeof(long long int) << endl;
+
+	return 0;
+
+}
+
+
+
